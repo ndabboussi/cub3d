7ec597ae4bcf6bffde4e7dc3e633d96f32fb8c7d
Nina Dbbs <dabboussi.nina@gmail.com>
2025-07-01 13:08:19 +0200

    get_textures created + ray_cast modified to handle textures

--- src/init.c
+++ src/init.c
@@ -12,10 +12,38 @@
 
 #include "cub3d.h"
 
-// void	init_map_textures(t_game *game)
-// {
+void	get_textures(t_game *game, t_texture *texture, char *path)
+{
+	int	width;
+	int	height;
 
-// }
+	texture->img = mlx_xpm_file_to_image(game->window.mlx_ptr, path, \
+					&width, &height);
+	if (!texture->img)
+	{
+		printf("Error: Could not load texture at path: %s\n", path);
+		exit(EXIT_FAILURE);
+	}
+	texture->addr = mlx_get_data_addr(texture->img, &texture->bits_per_pixel, \
+					&texture->line_length, &texture->endian);
+	if (!texture->addr)
+	{
+		printf("Error: Could not get data address for texture: %s\n", path);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	init_textures(t_game *game)
+{
+	game->texture.no_texture = ft_strtrim(game->texture.no_texture, " \n\t");
+	game->texture.so_texture = ft_strtrim(game->texture.so_texture, " \n\t");
+	game->texture.we_texture = ft_strtrim(game->texture.we_texture, " \n\t");
+	game->texture.ea_texture = ft_strtrim(game->texture.ea_texture, " \n\t");
+	get_textures(game, &game->no_texture, game->texture.no_texture);
+	get_textures(game, &game->so_texture, game->texture.so_texture);
+	get_textures(game, &game->we_texture, game->texture.we_texture);
+	get_textures(game, &game->ea_texture, game->texture.ea_texture);
+}
 
 void	init_window(t_game *game)
 {
@@ -45,6 +73,6 @@ void	init_window(t_game *game)
 		&game->window.bits_per_pixel, &game->window.line_length, \
 		&game->window.endian);
 	//init_map_textures(game);
-	//init_textures(game);
+	init_textures(game);
 	//init_colors(game);
 }
